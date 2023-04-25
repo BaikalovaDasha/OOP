291ca47538cc93ba994cfe695e80655878d3965e
BaikalovaDasha <baikalova0207@mail.ru>
2023-04-25 15:07:30 +0700

    feature (Program) : возможность вывода списка, состоящего из 7 рандомных персон

--- LR_1/LR_1/Program.cs
+++ LR_1/LR_1/Program.cs
@@ -13,10 +13,14 @@ namespace LR_2
         /// </summary>
         public static void Main()
         {
+            Console.WriteLine("Создание списка из 7 человек, состоящего из детей" +
+                " и взрослых.\n Для вывода информации о персоны нажмите любую клавишу.");
+            Console.WriteLine();
+            Console.ReadKey();
             var personlist = new PersonList();
             var randomList = new Random();
 
-            for (var i = 0; i < 8; i++)
+            for (var i = 0; i < 7; i++)
             {
                 PersonBase rndPerson = randomList.Next(2) == 0
                     ? RandomPersonGenerator.CreateRandomAdult()
@@ -29,7 +33,12 @@ namespace LR_2
                 Console.WriteLine($"\tПерсона №{i + 1}");
                 Console.WriteLine(personlist[i].GetInfo);
                 Console.WriteLine();
+                Console.ReadKey();
             }
+
+            Console.WriteLine("Вывод информации о 4 человека в списке");
+            Console.WriteLine();
+            Console.WriteLine(personlist[3].GetInfo);
 
         }
     }
