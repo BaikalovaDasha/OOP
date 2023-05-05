880a46abccbe555c57a15b6bbc8362cec80a23be
BaikalovaDasha <baikalova0207@mail.ru>
2023-05-05 10:27:59 +0700

    feature (program) :  изменение

--- LR_3/LR_3/Program.cs
+++ LR_3/LR_3/Program.cs
@@ -13,9 +13,37 @@ namespace ConsoleApp
         /// </summary>
         private static void Main()
         {
-            Console.WriteLine("Программ для расчёта затраченных калорий" +
-                " в зависимости от вида упражнений:");
-            ConsoleAddExercises.AddExercises();
+            Console.WriteLine("\t\tВас приветсвует программа расчёта объёма фигур.\n");
+
+            while (true)
+            {
+
+                Console.Write("Хотите расчитать объём фигуры - введите 1.\n" +
+                    "Хотите закончить выполнение программы - введите 2.\nВведите: ");
+                bool isParsed = short.TryParse(Console.ReadLine(),
+                            out short actionNumber);
+                if (!isParsed)
+                {
+                    Console.WriteLine("Невозможно понять что вы хотите.");
+                }
+                switch (actionNumber)
+                {
+                    case 1:
+                        {
+                            ConsoleAddExercises.AddExercises();
+                            break;
+                        }
+                    case 2:
+                        {
+                            return;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Введите число 1 или 2!");
+                            break;
+                        }
+                };
+            }
         }
     }
 }
